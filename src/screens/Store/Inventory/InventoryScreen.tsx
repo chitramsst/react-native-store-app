1ce9919cfe0e4627e904926016797509758df2a4
chitramsst <chitra.msst@gmail.com>
2024-06-18 11:08:51 +0530

    inventory

--- src/screens/Store/Inventory/InventoryScreen.tsx
+++ src/screens/Store/Inventory/InventoryScreen.tsx
@@ -24,40 +24,46 @@ export default function OrdersScreen({ navigation }) {
     navigation.navigate('OrderDetail');
   };
 
+  const data = [
+    { id: 1, title: 'Item 1' },
+    { id: 2, title: 'Item 2' },
+    { id: 3, title: 'Item 3' },
+    { id: 4, title: 'Item 4' },
+    { id: 5, title: 'Item 5' },
+    { id: 6, title: 'Item 6' }, // Add more items if needed for 5 rows
 
-  const renderItem = ({ item }: { item: string }) => {
-    let content;
-    if (item === 'GT-05') {
-      content = (
-        <Pressable style={styles.pressableItem} onPress={() => setModalMenuVisible(true)} className="m-2  p-2 bg-[#FB814B] border-0.5 h-24 w-[102px] rounded-lg border-black/20" >
-          <Text style={styles.pressableText} className="text-white">{item}</Text>
-          <View className="flex flex-row justify-center items-center mt-3 bg-[#e16d3b] rounded-lg py-[0.5px] mx-3">
-            <Pressable className='w-4 h-4 flex items-center justify-center '>
-              <Icon name="clock" size={10} color="#FFFFFF" />
-            </Pressable>
-            <Text className="text-[10px] text-[#FFFFFF] font-bold">0 min</Text>
+  ];
+
+  const renderItem = ({ item }) => (
+    <View className="w-[48%] m-1 bg-white max-h-32 min-h-32 rounded-lg flex flex-row justify-center items-center mt-3">
+      <View className="w-full h-full flex justify-center p-2">
+        <View className="justify-between flex flex-row items-center">
+          <View className="flex flex-row m-1 justify-center items-center space-x-4">
+            <Text className="text-gray-400 font-semibold text-xs">PIZ001</Text>
           </View>
-          <View className="flex flex-row justify-center items-center mt-3 bg-[#e16d3b] rounded-lg py-[0.5px] mx-3">
-            <Pressable className='w-4 h-4 flex items-center justify-center '>
-              <Icon name="dollar" size={10} color="#FFFFFF" />
-            </Pressable>
-            <Text className="text-[10px] text-[#FFFFFF] font-bold">4215.26</Text>
+          <View className="flex  flex-row justify-start items-center px-2 py-[2px]   m-1  text-white">
+            <Switch
+              trackColor={{ false: "#767577", true: "#3FDD78" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#FFFFFF"}
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
           </View>
-        </Pressable>
-      );
-    } else {
-      content = (
-        <Pressable style={styles.pressableItem} onPress={() => handlePress(item)} className="m-2  p-2 bg-white border-0.5 h-24 w-[102px] rounded-lg border-black/20">
-          <Text style={styles.pressableText}>{item}</Text>
-        </Pressable>
-      );
-    }
-    return (
-      <View>
-        {content}
+        </View>
+        <View className="flex flex-row justify-start items-center  pb-1 space-x-1 px-1">
+          <Text className="text-xs font-semibold text-gray-400 mt-1"> Margherita Pizza </Text>
+        </View>
+        <View className="flex flex-row justify-end items-center  pb-1 space-x-1 px-1">
+          <Text className="text-xs font-semibold text-gray-400 mt-1"> $61.00 </Text>
+        </View>
+        <View className="absolute bottom-0 w-full rounded-t-lg flex justify-center items-center">
+          <View className="border-[3px] bottom-0 border-[#3FDD78] rounded-t-lg w-[50%]"></View>
+        </View>
       </View>
-    );
-  };
+    </View>
+  );
+
+
   return (
 
     <View className="w-full h-full">
@@ -67,9 +73,7 @@ export default function OrdersScreen({ navigation }) {
           <View className="w-full flex flex-col justify-center items-center py-3">
             <Text className="text-md font-black text-black"> Inventory </Text>
           </View>
-
           {/* search section */}
-
           <View className='flex flex-row w-full justify-center items-center space-x-1' >
 
             <View className='flex flex-row mt-3 items-center bg-white rounded-full mb-1 w-11/12 h-10' >
@@ -88,71 +92,20 @@ export default function OrdersScreen({ navigation }) {
               <Image source={require('../../../assets/images/search-menu.png')} className='h-9 w-9' />
             </View>
           </View>
-
-          <View className="w-full flex justify-center items-center flex-row space-x-2 mt-2">
-
-            {/* TOTAL ORDERS */}
-            <View className="w-1/2 bg-white max-h-32 min-h-32 rounded-lg flex flex-row justify-center items-center mt-3">
-              <View className="w-full h-full flex justify-center p-2">
-                <View className="justify-between flex flex-row items-center">
-                  <View className="flex flex-row m-1 justify-center items-center space-x-4">
-                    <Text className="text-gray-400 font-semibold text-xs">PIZ001</Text>
-                  </View>
-                  <View className="flex  flex-row justify-start items-center px-2 py-[2px]   m-1  text-white">
-                    <Switch
-                      trackColor={{ false: "#767577", true: "#3FDD78" }}
-                      thumbColor={isEnabled ? "#FFFFFF" : "#FFFFFF"}
-                      onValueChange={toggleSwitch}
-                      value={isEnabled}
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row justify-start items-center  pb-1 space-x-1 px-1">
-                  <Text className="text-xs font-semibold text-gray-400 mt-1"> Margherita Pizza </Text>
-                </View>
-                <View className="flex flex-row justify-end items-center  pb-1 space-x-1 px-1">
-                  <Text className="text-xs font-semibold text-gray-400 mt-1"> $61.00 </Text>
-                </View>
-                <View className="absolute bottom-0 w-full rounded-t-lg flex justify-center items-center">
-                  <View className="border-[3px] bottom-0 border-[#3FDD78] rounded-t-lg w-[50%]"></View>
-                </View>
-              </View>
-            </View>
-            <View className="w-1/2 bg-white max-h-32 min-h-32 rounded-lg flex flex-row justify-center items-center mt-3">
-              <View className="w-full h-full flex justify-center p-2">
-                <View className="justify-between flex flex-row items-center">
-                  <View className="flex flex-row m-1 justify-center items-center space-x-4">
-                    <Text className="text-gray-400 font-semibold text-xs">PIZ001</Text>
-                  </View>
-                  <View className="flex  flex-row justify-start items-center px-2 py-[2px]   m-1  text-white">
-                    <Switch
-                      trackColor={{ false: "#767577", true: "#3FDD78" }}
-                      thumbColor={isEnabled ? "#FFFFFF" : "#FFFFFF"}
-                      onValueChange={toggleSwitch}
-                      value={isEnabled}
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row justify-start items-center  pb-1 space-x-1 px-1">
-                  <Text className="text-xs font-semibold text-gray-400 mt-1"> Margherita Pizza </Text>
-                </View>
-                <View className="flex flex-row justify-end items-center  pb-1 space-x-1 px-1">
-                  <Text className="text-xs font-semibold text-gray-400 mt-1"> $61.00 </Text>
-                </View>
-                <View className="absolute bottom-0 w-full rounded-t-lg flex justify-center items-center">
-                  <View className="border-[3px] bottom-0 border-[#3FDD78] rounded-t-lg w-[50%]"></View>
-                </View>
-              </View>
-            </View>
-          </View>
-
           {/*  body section */}
-        
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            horizontal = {false}
+            numColumns={2} // Set number of columns to 2
+            keyExtractor={item => item.id} // Extract unique key for each item
+          />
         </View>
       </View>
       <View className='absolute bottom-10 right-10 bg-[#FB814B] w-10 h-10 rounded-full flex justify-center items-center '>
         <Icon name="plus" size={14} color="#FCFCFC" />
       </View>
+
     </View >
   );
 }
