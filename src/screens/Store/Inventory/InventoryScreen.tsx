a8670821bfb35b8a29f8fe941b03ada30f315507
chitramsst <chitra.msst@gmail.com>
2024-06-19 00:00:03 +0530

    Manage Category

--- src/screens/Store/Inventory/InventoryScreen.tsx
+++ src/screens/Store/Inventory/InventoryScreen.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import IconMaterial from 'react-native-vector-icons/MaterialIcons';
 import IconFontisto from 'react-native-vector-icons/Fontisto';
-
+import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function OrdersScreen({ navigation }) {
 
@@ -17,15 +17,41 @@ export default function OrdersScreen({ navigation }) {
     return textWithoutDollar.replace(/\./, "").length;
   };
 
-  
+
+
   const text = "$512.64";
   const textLength = getTextLength(text);
 
+  const [menuVisible, setMenuVisible] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const Checkbox = ({ label, checked, onPress }) => (
+    <TouchableOpacity style={styles.checkboxContainer} onPress={onPress}>
+        {checked && <IconMaterialIcons name="check-box" size={15} color="#FB814B" />}
+        {!checked && <IconMaterialIcons name="check-box-outline-blank" size={15} color="#9A9FA5" />}
+        <Text className="text-[10px]  text-[#141718] ml-2">{label}</Text>
+    </TouchableOpacity>
+);
+
+const [checkboxes, setCheckboxes] = useState([
+  { label: 'Pepperoni', checked: false },
+  { label: 'Mushrooms', checked: true },
+  { label: 'Onions', checked: false },
+  { label: 'Bell peppers', checked: false },
+]);
   const handlePress = () => {
     navigation.navigate('Dashboard');
   };
 
+  const toggleCheckbox = (index) => {
+    const updatedCheckboxes = checkboxes.map((checkbox, i) =>
+        i === index ? { ...checkbox, checked: !checkbox.checked } : checkbox
+    );
+    setCheckboxes(updatedCheckboxes);
+};
 
   const handlePressNewItem = () => {
     navigation.navigate('AddNewItem');
@@ -40,12 +66,12 @@ export default function OrdersScreen({ navigation }) {
     { id: 6, title: 'Item 6' }, // Add more items if needed for 5 rows
     { id: 7, title: 'Item 3' },
     { id: 8, title: 'Item 4' },
-   
+
   ];
 
   const renderItem = ({ item }) => (
-    <View className="w-[48%] mr-3 bg-white max-h-32 min-h-32 rounded-lg flex flex-row justify-center items-center mt-3">
-      <View className="w-full h-full flex justify-center p-2">
+    <View className="w-[49%] mr-2 bg-white max-h-32 min-h-32 rounded-lg flex flex-row justify-center items-center mt-3">
+      <View className="w-full h-full flex justify-center ">
         <View className="justify-between flex flex-row items-center">
           <View className="flex flex-row m-1 justify-center items-center space-x-4">
             <Text className="text-gray-400 font-semibold text-xs">PIZ001</Text>
@@ -80,12 +106,12 @@ export default function OrdersScreen({ navigation }) {
       <View className="w-full h-full flex bg-black/7" style={styles.container}  >
         <View className=" p-5 w-full">
           <View className="flex flex-row justify-center items-center">
-        <Pressable className='w-6 h-6 rounded-full flex items-center justify-center ' onPress={() => handlePress()}>
-                            <Icon name="arrow-left" size={14} color="#6F767E" />
-                        </Pressable>
-          <View className="w-full flex flex-col justify-center items-center py-3">
-            <Text className="text-md font-black text-black"> Inventory </Text>
-          </View>
+            <Pressable className='w-6 h-6 rounded-full flex items-center justify-center ' onPress={() => handlePress()}>
+              <Icon name="arrow-left" size={14} color="#6F767E" />
+            </Pressable>
+            <View className="w-full flex flex-col justify-center items-center py-3">
+              <Text className="text-md font-black text-black"> Inventory </Text>
+            </View>
           </View>
           {/* search section */}
           <View className='flex flex-row w-full justify-center items-center space-x-1' >
@@ -102,18 +128,45 @@ export default function OrdersScreen({ navigation }) {
                 placeholderTextColor="#98A2B3"
               />
             </View>
-            <View className='flex flex-row mt-3 items-center bg-white rounded-full mb-1'>
+            <Pressable className='flex flex-row mt-3 items-center bg-white rounded-full mb-1' onPress={toggleMenu}>
               <Image source={require('../../../assets/images/search-menu.png')} className='h-9 w-9' />
-            </View>
+            </Pressable>
+            {menuVisible && (
+              <View style={styles.dropdownMenu} className="w-64">
+                {/* Add your dropdown menu items here */}
+                <Pressable onPress={() => console.log('Option 1 selected')}>
+                  <View style={styles.menuItem}>
+                    {checkboxes.map((checkbox, index) => (
+                                <View className="flex flex-row justify-between items-center w-full" key={checkbox.label}>
+                                    <View className="flex flex-row items-center justify-between space-x-1 w-full">
+                                    <Text className="text-[10px] font-bold text-[#141718]">{checkbox.label}</Text>
+                                    <View className="justify-center items-center flex pt-2 mt-1">
+                                        <Checkbox
+                                            key={index}
+                                            checked={checkbox.checked}
+                                            onPress={() => toggleCheckbox(index)}
+                                        />
+                                        </View>
+                                    </View>
+                                </View>
+                            ))}
+                  </View>
+                  <Pressable className="rounded-lg bg-[#FB814B] text-[#FCFCFC] px-3 py-3 flex justify-center items-center" onPress={() => handlePress()}>
+                                <Text className="text-[12px] text-white font-bold"> Manage Category </Text>
+                            </Pressable>
+                </Pressable>
+              </View>
+            )}
           </View>
           {/*  body section */}
           <FlatList
             data={data}
             renderItem={renderItem}
-            horizontal = {false}
+            horizontal={false}
             numColumns={2} // Set number of columns to 2
             keyExtractor={item => item.id} // Extract unique key for each item
-              className="w-full p-1  h-[80%]"
+            className="w-full  h-[80%]"
+            columnWrapperStyle={styles.columnWrapper}
           />
         </View>
       </View>
@@ -159,5 +212,24 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 10,
     right: 10,
+  },
+  dropdownMenu: {
+    position: 'absolute',
+    top: 60,
+    right: 8,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 8,
+    elevation: 5, // for shadow effect on Android
+    shadowColor: '#000', // for shadow effect on iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    zIndex: 1000,
+  },
+  menuItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
   },
 })
